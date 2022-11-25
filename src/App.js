67021fdcc2a1fb6c015bc9ff694f38043e62fc92
Sonic7911 <sonicgautam111@gmail.com>
2022-11-25 08:52:19 +0545

    weather fixing logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
 
 function App() {
+
+  const[notification, setNotification] = useState(0);
+  useEffect(()=>
+  { 
+      if(notification>0)
+      document.title="You have "+notification+" notifications"
+  })
+
+  function sendNotification()
+  {
+    setNotification(notification+1)
+  }
+
   return (
  
     <div className='App'>
-        <Game />
+        <button onClick={sendNotification}>Send Notification</button>
         
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Weather from './components/Weather.js';
 import reportWebVitals from './reportWebVitals';
+import AnimeQuotes from './components/AnimeQuotes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className ="App">
+       <Weather />
+    </div>
+   
   </React.StrictMode>
 );
 
